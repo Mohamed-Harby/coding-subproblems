0108aa1a606ac2f91b90155a5cf6a0563223c1ce
Mohamed Harby <71990569+MoHarby@users.noreply.github.com>
2021-01-16 16:23:51 +0200

    Update Factorial Division.cpp

--- Factorial Division.cpp
+++ Factorial Division.cpp
@@ -9,18 +9,16 @@ double factorialDivision(int n,int m){  // O(n*m)
 	double res = 1;
 	for(int i=2;i <= n;i++) numerator.push_back(i);
 	for(int i=2;i <= m;i++) denominator.push_back(i);
-
-	for(int i=0;i<m-1;i++){
-		for(int j=0;j<n-1;j++){
-			if(__gcd(denominator[i],numerator[j]) == denominator[i]){
-				numerator[j]/=denominator[i];
-				denominator[i] = 1;
-				break;
+	int minimum = min(n,m);
+	--minimum;
+	for(int i=0;i<minimum;i++){
+			if(denominator[i]==numerator[i]){
+				denominator[i]=numerator[i]=1;
 			}
-		}
+			else break;
 	}
 	for(int i=0;i<n-1;i++) res*=numerator[i];
-	for(int i=0;i<m-1;i++) res*=denominator[i];
+	for(int i=0;i<m-1;i++) res/=denominator[i];
 
 	return res;
 }
@@ -31,7 +29,12 @@ int main() {
 	cout << factorialDivision(17, 16) << endl;    // 17
 
 	cout << factorialDivision(13, 5) << endl;   // 5.18918e+07
-  
-  cout << factorialDivision(1000, 999) << endl; // 1000
+
+	cout << factorialDivision(1000, 999) << endl; // 1000
+
+	cout << factorialDivision(3, 55) << endl; // 4.72575e-73
+
+	return 0;
 }
+
 // https://ideone.com/ZdC7cb
